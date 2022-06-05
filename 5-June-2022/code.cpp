4a56d9e8a0d8224576fb76980a4bf3f0fcb4d5f7
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-06-05 13:59:56 +0700

    Update code

--- 5-June-2022/code.cpp
+++ 5-June-2022/code.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -9,35 +10,18 @@ int main() {
     double b = 0.9751669964155007;
     double result = a + b;
 
+    cout << result << endl;
+    cout << setprecision(50) << result << endl; 
+
+    // Wrong way
+    if (result == 1)
+        cout << "Equal to 1" << endl;
+    else
+        cout << "Not equal to 1" << endl;
+
+    // Correct way
     if (abs(1 - result) < EPS)
         cout << "Equal to 1" << endl;
     else
         cout << "Not equal to 1" << endl;
 }
-
-// https://floating-point-gui.de/errors/comparison/
-
-// #include <iostream>
-
-// using namespace std;
-
-// int main() {
-//     double a = 0.0248330035844995;
-//     double b = 0.9751669964155007;
-//     double result = a + b;
-
-//     cout << result << endl;
-// }
-
-// #include <iostream>
-// #include <iomanip>
-
-// using namespace std;
-
-// int main() {
-//     double a = 0.0248330035844995;
-//     double b = 0.9751669964155007;
-//     double result = a + b;
-
-//     cout << setprecision(50) << result;
-// }
